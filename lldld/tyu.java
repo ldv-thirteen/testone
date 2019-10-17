45e23340c9eda42e15cfbbcb8767b825b73db767
ldv_13 <ldv.thirteen@bk.ru>
2019-10-17 17:14:40 +0300

    third blenb

---
+++ lldld/tyu.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class tyu {
+    public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int a = in.nextInt();
+            System.out.println(a);
+
+        }
+    }
+
