3a9948db1bc2d8ec2636405c874a8319347c2d12
ldv_13 <ldv.thirteen@bk.ru>
2019-10-17 17:24:26 +0300

    t

--- lldld/tyu.java
+++ lldld/tyu.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 public class tyu {
     public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            int a = in.nextInt();
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        if (a > b && a > c) {
             System.out.println(a);
 
         }
     }
+}
 
