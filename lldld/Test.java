22d315d3e6cf68817f5858a3c02961b63318e39c
ldv_13 <ldv.thirteen@bk.ru>
2019-10-17 18:01:34 +0300

    xvacxv

--- lldld/Test.java
+++ lldld/Test.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
+
 public class Test {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int c = in.nextInt();
-        if (a > b && a > c) {
-            System.out.println(a);
-        } else if (a > b && c > a) {
-            System.out.println(c);
-
-        } else if (a < b && c > a) {
-            System.out.println();
+        int a, b, c;
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+        if (a > b && b > c) {
+            System.out.print(a);
+        } else if (a > c && c > b) {
+            System.out.print(a);
+        } else if (b > c && c > a) {
+            System.out.print( b);
+        } else if (b > a && a > c) {
+            System.out.print( b);
+        } else if (c > b && b > a) {
+            System.out.print(c);
+        } else if (c > a && a > b) {
+            System.out.print(c);
         }
     }
-
-
-
-        }
-
-
+}
